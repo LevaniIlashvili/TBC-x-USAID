404598e2012627c9b5cfd9701b8a6f252b9a47b8
LevaniIlashvili <levaniko900@gmail.com>
2024-01-24 14:47:28 +0400

    fluid navbar

--- script.js
+++ script.js
@@ -5,12 +5,27 @@ const navbar = document.querySelector(".navbar");
 const heroSection = document.querySelector(".hero-section");
 const heroSectionOffset = heroSection.offsetTop;
 
+let prevScrollPos = window.pageYOffset;
+
 window.addEventListener("scroll", () => {
+  console.log(window.scrollY, heroSectionOffset - navbar.clientHeight);
   if (window.scrollY > heroSectionOffset - navbar.clientHeight) {
     navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
   }
+
+  let currentScrollPos = window.pageYOffset;
+
+  if (prevScrollPos > currentScrollPos) {
+    // Scrolling up
+    navbar.style.top = "0";
+  } else {
+    // Scrolling down
+    navbar.style.top = `-${navbar.offsetHeight}px`;
+  }
+
+  prevScrollPos = currentScrollPos;
 });
 
 // Navbar toggle
