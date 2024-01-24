e960751aa029e5a8b977a1b171ff2056a1b06991
LevaniIlashvili <levaniko900@gmail.com>
2024-01-24 14:34:59 +0400

    add responsivness

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // Navbar
-const navbar = document.querySelector("nav");
+const navbar = document.querySelector(".navbar");
 const heroSection = document.querySelector(".hero-section");
 const heroSectionOffset = heroSection.offsetTop;
 
@@ -11,4 +11,23 @@ window.addEventListener("scroll", () => {
   } else {
     navbar.classList.remove("sticky");
   }
+});
+
+// Navbar toggle
+let isNavbarOpen = false;
+
+const hamburgerLogos = document.querySelectorAll(".hamburger-menu-logo");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+
+hamburgerLogos.forEach((hamburgerLogo) => {
+  hamburgerLogo.addEventListener("click", () => {
+    console.log("clicked");
+    if (!isNavbarOpen) {
+      hamburgerMenu.classList.add("open");
+      isNavbarOpen = true;
+    } else {
+      hamburgerMenu.classList.remove("open");
+      isNavbarOpen = false;
+    }
+  });
 });
